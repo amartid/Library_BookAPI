cbde089e7d09dc1cbbdec5e9ace1dfc3897e7561
amartid <64313984+amartid@users.noreply.github.com>
2023-10-01 13:17:05 +0300

    custom logs - SeriLog

--- Library_BookAPI/Controllers/BookAPIClass.cs
+++ Library_BookAPI/Controllers/BookAPIClass.cs
@@ -7,6 +7,9 @@ using System;
 using Library_BookAPI.Models;
 using Library_BookAPI.Models.Dto;
 using Library_BookAPI.Data;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.Extensions.Logging;
 
 namespace Library_BookAPI.Controllers
 {
@@ -22,33 +25,134 @@ namespace Library_BookAPI.Controllers
 
     // Error Handling: It also configures the controller to return problem details in response to common error scenarios,
     // such as 400 Bad Request for model validation errors and 500 Internal Server Error for unhandled exceptions.
-    public class BookAPIClass : ControllerBase
+    public class BookAPIController : ControllerBase
     // ControllerBase: provides a set of methods and properties that
     // simplify the implementation of controller actions.
     {
+        private readonly ILogger<BookAPIController> _logger;
+        // Constructor that receives an ILogger<BookAPIController> as a dependency
+        public BookAPIController(ILogger<BookAPIController> logger)
+        {
+            _logger = logger; // Assign the provided logger to the private _logger field
+        }
+
+
+
+
+
         [HttpGet] // GET endpoint ! RETURNS 1 RECORD
         public ActionResult<IEnumerable<BookDTO>> GetBooks()
         {
+            _logger.LogInformation("Getting all books"); 
             return Ok(BookStore.bookList);
         }
         
-        [HttpGet("{id:int}")] // GET endpoint ! RETURNS MULTIPLE RECORD
+        [HttpGet("{id:int}", Name = "GetBook")] // GET endpoint ! RETURNS MULTIPLE RECORD
         [ProducesResponseType(StatusCodes.Status200OK)] // DEFINE RESPONSES
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-//      [ProducesResponseType(200, Type = typeof(BookDTO))] // DEFINE RESPONSES
-
+        // [ProducesResponseType(200, Type = typeof(BookDTO))] 
         public ActionResult<BookDTO> GetBook(int id)
         {
             if(id==0)
             {
+                _logger.LogError("Get Book Error with Id" + id);
                 return BadRequest(); // response status is 400 = BAD REQUEST
             }
-            var book = BookStore.bookList.FirstOrDefault(u => u.Id == id);
+            var book = BookStore.bookList.FirstOrDefault(u => u.Id == id);//link operation
             if (book == null)
                 return NotFound(); // response status is 404 = NOT FOUND
 
-            return Ok(); //link operation
+            return Ok(book);
+        }
+        
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)] // DEFINE RESPONSES
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<BookDTO> CreateBook([FromBody] BookDTO bookDTO)
+        {
+            //if (!ModelState.IsValid)
+            //{
+            //    return BadRequest(ModelState);
+            //}
+            // how to add custom validation:
+            if (BookStore.bookList.FirstOrDefault(u => u.Title.ToLower() == bookDTO.Title.ToLower()) != null)
+            {
+                ModelState.AddModelError("Custom Error", "Book already exists !");
+                return BadRequest(ModelState);
+            }
+                
+            if (bookDTO == null)  // valid bookDTO object is provided in the request body
+            {
+                return BadRequest(bookDTO);
+            }
+            if (bookDTO.Id > 0) // ID less than or equal to zero. When creating the id should be zero
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+            bookDTO.Id = BookStore.bookList.OrderByDescending(u => u.Id).FirstOrDefault().Id + 1; // id gets max value
+            BookStore.bookList.Add(bookDTO); //bookDTO added to List
+            return CreatedAtRoute("GetBook", new { id = bookDTO.Id },bookDTO);
+        }
+        [HttpDelete("{id:int}", Name = "DeleteBook")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)] // DEFINE RESPONSES
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult DeleteBook(int id) //with IActionResult you do not define the action type !
+        {
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+            var book = BookStore.bookList.FirstOrDefault(u => u.Id == id);
+            if (book == null)  // valid book object is provided in the request body
+            {
+                return NotFound();
+            }
+            BookStore.bookList.Remove(book);
+            return NoContent();
+        }
+        [HttpPut("{id:int}", Name = "UpdateBook")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)] // DEFINE RESPONSES
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult UpdateBook(int id, [FromBody] BookDTO bookDTO)
+        {
+            if (bookDTO == null || id != bookDTO.Id)
+            {
+                return BadRequest();
+            }
+            var book = BookStore.bookList.FirstOrDefault(u => u.Id == id);
+            book.Title = bookDTO.Title;
+            book.Author = bookDTO.Author;
+            book.ISBN = bookDTO.ISBN;
+            book.YearPublished = bookDTO.YearPublished;
+            book.Genre = bookDTO.Genre;
+            return NoContent();
+
+
+        }
+
+        [HttpPatch("{id:int}", Name = "UpdatePartialBook")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)] // DEFINE RESPONSES
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult UpdatePartialBook(int id, JsonPatchDocument<BookDTO> patchDTO)
+        {
+            if (patchDTO == null || id == 0)
+            {
+                return BadRequest();
+            }
+            var book = BookStore.bookList.FirstOrDefault(u => u.Id == id);
+            if (book == null)
+            {
+                return BadRequest();
+            }
+            patchDTO.ApplyTo(book, ModelState);
+            if (ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            return NoContent();
         }
     }  
 }
--- Library_BookAPI/Models/Dto/BookDTO.cs
+++ Library_BookAPI/Models/Dto/BookDTO.cs
@@ -1,9 +1,13 @@
-﻿namespace Library_BookAPI.Models.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Library_BookAPI.Models.Dto
 {
     public class BookDTO
     {
         // properties of Book
         public int Id { get; set; }
+        [Required]
+        [MaxLength(40)]
         public string Title { get; set; }
         public string Author { get; set; }
         public string ISBN { get; set; }
